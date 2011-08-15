23a2aa2425f778de583297e376a0af34f0264b3b
Peter Bengtsson <mail@peterbe.com>
2011-08-15 15:46:34 +0100

    improved test related to timezone

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,4 @@
+import os
 import re
 from urlparse import urlparse
 import datetime
@@ -257,7 +258,11 @@ class UsersTest(TestCase):
         then = datetime.datetime.strptime(date, '%d-%b-%Y').date()
         today = datetime.date.today()
         days = settings.SESSION_COOKIE_AGE / 24 / 3600
-        eq_((then - today).days - 1, days)
+        try:
+            eq_((then - today).days, days)
+        except AssertionError:
+            print "Make sure your settings.TIME_ZONE matches your OS clock"
+            raise
 
     def test_login_by_email(self):
         url = reverse('users.login')
